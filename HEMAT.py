f9a283f468fc3a4c39e91b8ce825d429db2c4b62
HEMAT KHAN <88194852+HemtKhan@users.noreply.github.com>
2022-09-05 11:45:50 +0430

    Create HEMAT.py

---
+++ HEMAT.py
@@ -0,0 +1,14 @@
+import os, platform
+os.system("pkg install play-audio")
+os.system('git pull')
+
+
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+    os.system("play-audio WELCOME_TO_HEMAT_RANDOM_CLONE_TOOL.mp3")
+    from Random import xyz
+    xyz()
